d974e6ac9fa5b1096d1901691a5e71be72f52e36
Lasse Spindler Mørk <Baizey@users.noreply.github.com>
2022-09-24 00:17:33 +0200

    7.1.0 add 'realValue' mock strategy, which will allow a dependency (or its property) to act as normal

--- src/ServiceCollection/mockUtils.ts
+++ src/ServiceCollection/mockUtils.ts
@@ -6,136 +6,147 @@ import { ServiceCollection } from './ServiceCollection'
 import { DependencyFactory, Key } from './types'
 
 export enum MockStrategy {
-	/**
-	 * ONLY HERE FOR DOCUMENTATION PURPOSES, DO NOT USE IN CODE
-	 *
-	 * Default behaviour for any dependency/dependency property given in the ProviderMock that isn't a MockStrategy
-	 * Getter usage will return most recent dummy value (either the initial value, or a later set value)
-	 * Setter usage will set new dummy value
-	 */
-	dummyStub = 'dummyStub',
-	
-	/**
-	 * This is the default behaviour unless otherwise explicitly stated
-	 *
-	 * Getter usage will return null
-	 * Setter usage will do nothing
-	 */
-	nullStub = 'nullStub',
-	
-	/**
-	 * Getter usage will throw NotMockedException(dependency, prop, usage-type)
-	 * Setter usage will throw NotMockedException(dependency, prop, usage-type)
-	 */
-	exceptionStub = 'exceptionStub',
+  /**
+   * ONLY HERE FOR DOCUMENTATION PURPOSES, DO NOT USE IN CODE
+   *
+   * Default behaviour for any dependency/dependency property given in the ProviderMock that isn't a MockStrategy
+   * Getter usage will return most recent dummy value (either the initial value, or a later set value)
+   * Setter usage will set new dummy value
+   */
+  dummyStub = 'dummyStub',
+
+  /**
+   * This is the default behaviour unless otherwise explicitly stated
+   *
+   * Getter usage will return null
+   * Setter usage will do nothing
+   */
+  nullStub = 'nullStub',
+
+  /**
+   * Getter usage will throw NotMockedException(dependency, prop, usage-type)
+   * Setter usage will throw NotMockedException(dependency, prop, usage-type)
+   */
+  exceptionStub = 'exceptionStub',
+
+  /**
+   * Returns the real value, anything with this strategy won't be mocked at all
+   */
+  realValue = 'realValue',
 }
 
-type PartialNested<T> = { [key in keyof T]?: T[key] extends object ? PartialNested<T[key]> : T[key] }
-type PropertyMock<T> = { [key in keyof T]?: PartialNested<T[key]> | MockStrategy | null | (() => null) }
+type PartialNested<T> = { [key in keyof T]?: T[key] extends object ? PartialNested<T[key]> : T[key] };
+type PropertyMock<T> = { [key in keyof T]?: PartialNested<T[key]> | MockStrategy | null | ( () => null ) };
 type DependencyMock<E, K extends keyof E> =
-	| Partial<PropertyMock<E[K]>>
-	| DependencyFactory<Partial<PropertyMock<E[K]>>, any, E>
-	| MockStrategy
+  | Partial<PropertyMock<E[K]>>
+  | DependencyFactory<Partial<PropertyMock<E[K]>>, any, E>
+  | MockStrategy;
 export type ProviderMock<E> = { [key in keyof E]?: DependencyMock<E, key> };
 
-export function proxyLifetimes<E>(services: ServiceCollection<E>,
-                                  providerMock: MockStrategy | ProviderMock<E>,
-                                  defaultMock: MockStrategy = MockStrategy.nullStub) {
-	
-	if (typeof providerMock === 'string') { defaultMock = providerMock as MockStrategy }
-	const dependencyMock: ProviderMock<E> = (typeof providerMock === 'string') ? {} : providerMock
-	
-	const provider = services.build()
-	Object.values<ILifetime<unknown, E>>(provider.lifetimes)
-		.forEach((lifetime) => {
-			const propertyMock = dependencyMock[lifetime.name] ?? defaultMock
-			provider.lifetimes[lifetime.name] = proxyLifetime(lifetime, propertyMock, defaultMock)
-		})
-	return provider
+export function proxyLifetimes<E>(
+  services: ServiceCollection<E>,
+  providerMock: MockStrategy | ProviderMock<E>,
+  defaultMock: MockStrategy = MockStrategy.nullStub,
+) {
+  if ( typeof providerMock === 'string' ) {
+    defaultMock = providerMock as MockStrategy
+  }
+  const dependencyMock: ProviderMock<E> = typeof providerMock === 'string' ? {} : providerMock
+
+  const provider = services.build()
+  Object.values<ILifetime<unknown, E>>( provider.lifetimes ).forEach( ( lifetime ) => {
+    const propertyMock = dependencyMock[lifetime.name] ?? defaultMock
+    if ( propertyMock !== MockStrategy.realValue )
+      provider.lifetimes[lifetime.name] = proxyLifetime( lifetime, propertyMock, defaultMock )
+  } )
+  return provider
 }
 
 const provide = propertyOf<ILifetime<unknown, any>>().provide
 
 export function proxyLifetime<E>(
-	lifetime: ILifetime<unknown, E>,
-	dependencyMock: DependencyMock<E, keyof E>,
-	defaultMock: MockStrategy,
+  lifetime: ILifetime<unknown, E>,
+  dependencyMock: DependencyMock<E, keyof E>,
+  defaultMock: MockStrategy,
 ) {
-	const name = lifetime.name.toString()
-	return new Proxy(lifetime, {
-		get(target, prop: keyof ILifetime<unknown, E>) {
-			if (prop !== provide) return target[prop]
-			return (context: ScopedServiceProvider<E>) => {
-				if (context.depth === 1) return target.provide(context)
-				
-				const shadow = target.provide(context)
-				
-				switch (typeof dependencyMock) {
-					case 'string':
-						return mockDependency( name, shadow, {}, dependencyMock as MockStrategy )
-					case 'function':
-						return mockDependency( name,
-							shadow,
-							dependencyMock( context.proxy, undefined, context ),
-							defaultMock )
-					case 'object':
-						return mockDependency( name, shadow, dependencyMock, defaultMock )
-					default:
-						return mockDependency(name, shadow, {}, defaultMock)
-				}
-			}
-		},
-	})
+  const name = lifetime.name.toString()
+  return new Proxy( lifetime, {
+    get( target, prop: keyof ILifetime<unknown, E> ) {
+      if ( prop !== provide ) return target[prop]
+      return ( context: ScopedServiceProvider<E> ) => {
+        if ( context.depth === 1 ) return target.provide( context )
+
+        const shadow = target.provide( context )
+
+        switch ( typeof dependencyMock ) {
+          case 'string':
+            return mockDependency( name, shadow, {}, dependencyMock )
+          case 'function':
+            return mockDependency( name, shadow, dependencyMock( context.proxy, undefined, context ), defaultMock )
+          case 'object':
+            return mockDependency( name, shadow, dependencyMock, defaultMock )
+          default:
+            return mockDependency( name, shadow, {}, defaultMock )
+        }
+      }
+    },
+  } )
 }
 
-export function mockDependency<T extends object>(name: string,
-                                                 shadowInstance: any,
-                                                 propertyMock: PropertyMock<T>,
-                                                 defaultMock: MockStrategy): T {
-	function isFunc(prop: any) {
-		return typeof shadowInstance[prop] === 'function'
-	}
-	
-	function mockValue(prop: Key<T>, mockType: MockStrategy, type: Access) {
-		switch (mockType) {
-			case MockStrategy.exceptionStub:
-				if (!isFunc(prop)) throw new ShouldBeMockedDependencyError(name, prop, type)
-				return () => { throw  new ShouldBeMockedDependencyError(name, prop, 'function') }
-			case MockStrategy.nullStub:
-			default:
-				return isFunc(prop) ? (() => null) : null
-		}
-	}
-	
-	// noinspection JSUnusedGlobalSymbols
-	const proxy = new Proxy(propertyMock,
-		{
-			get(target, prop: Key<T>) {
-				const value = target[prop]
-				const mockType = (value as MockStrategy)
-				
-				if (prop in target && mockType in MockStrategy)
-					return mockValue(prop, mockType, 'get')
-				else if (prop in target || Object.getOwnPropertyDescriptor(target, prop)?.get)
-					return value
-				else
-					return mockValue(prop, defaultMock, 'get')
-			},
-			set(target, prop: Key<T>, newValue) {
-				const value = target[prop]
-				const mockType = (value as MockStrategy)
-				
-				if (prop in target && mockType in MockStrategy)
-					target[prop] = mockValue(prop, mockType, 'set')
-				else if (prop in target || Object.getOwnPropertyDescriptor(target, prop)?.set)
-					target[prop] = newValue
-				else
-					target[prop] = mockValue(prop, defaultMock, 'set')
-				
-				return true
-			},
-		},
-	) as PropertyMock<T>
-	
-	return proxy as T
+export function mockDependency<T extends object>(
+  name: string,
+  shadowInstance: any,
+  propertyMock: PropertyMock<T>,
+  defaultMock: MockStrategy,
+): T {
+  function isFunc( prop: any ) {
+    return typeof shadowInstance[prop] === 'function'
+  }
+
+  function mockValue( prop: Key<T>, mockType: MockStrategy, type: Access ) {
+    switch ( mockType ) {
+      case MockStrategy.realValue:
+        return shadowInstance[prop]
+      case MockStrategy.exceptionStub:
+        if ( !isFunc( prop ) ) throw new ShouldBeMockedDependencyError( name, prop, type )
+        return () => {
+          throw new ShouldBeMockedDependencyError( name, prop, 'function' )
+        }
+      case MockStrategy.nullStub:
+      default:
+        return isFunc( prop ) ? () => null : null
+    }
+  }
+
+  // noinspection JSUnusedGlobalSymbols
+  const proxy = new Proxy( propertyMock, {
+    get( target, prop: Key<T> ) {
+      const value = target[prop]
+      const mockType = value as MockStrategy
+
+      if ( prop in target && mockType in MockStrategy ) {
+        return mockValue( prop, mockType, 'get' )
+      } else if ( prop in target || Object.getOwnPropertyDescriptor( target, prop )?.get ) {
+        return value
+      } else {
+        return mockValue( prop, defaultMock, 'get' )
+      }
+    },
+    set( target, prop: Key<T>, newValue ) {
+      const value = target[prop]
+      const mockType = value as MockStrategy
+
+      if ( prop in target && mockType in MockStrategy ) {
+        target[prop] = mockValue( prop, mockType, 'set' )
+      } else if ( prop in target || Object.getOwnPropertyDescriptor( target, prop )?.set ) {
+        target[prop] = newValue
+      } else {
+        target[prop] = mockValue( prop, defaultMock, 'set' )
+      }
+
+      return true
+    },
+  } ) as PropertyMock<T>
+
+  return proxy as T
 }
--- test/ServiceCollection.test.ts
+++ test/ServiceCollection.test.ts
@@ -1,15 +1,15 @@
 import {
-	DuplicateDependencyError,
-	MockStrategy,
-	propertyOf,
-	scoped,
-	ScopedServiceProvider,
-	ServiceCollection,
-	Services,
-	ShouldBeMockedDependencyError,
-	singleton,
-	stateful,
-	transient,
+  DuplicateDependencyError,
+  MockStrategy,
+  propertyOf,
+  scoped,
+  ScopedServiceProvider,
+  ServiceCollection,
+  Services,
+  ShouldBeMockedDependencyError,
+  singleton,
+  stateful,
+  transient,
 } from '../src'
 import { ScopedLifetime } from '../src/Lifetime/ScopedLifetime'
 import { SingletonLifetime } from '../src/Lifetime/SingletonLifetime'
@@ -17,147 +17,167 @@ import { StatefulLifetime } from '../src/Lifetime/StatefulLifetime'
 import { TransientLifetime } from '../src/Lifetime/TransientLifetime'
 import { Context, dummy, dummyClass, UUID } from './testUtils'
 
-class Dummy {}
+class Dummy {
+}
 
 const services = propertyOf<ServiceCollection>()
 
 describe( services.add, () => {
-	test( 'add', () => {
-		const sut = Services().add( { a: singleton( Dummy ) } )
-		expect( sut.get( 'a' ) ).toBeTruthy()
-	} )
-	test( 'addSingleton', () => {
-		const sut = Services()
-		const spy = jest.spyOn( sut, 'add' )
-		const actual = sut.add( { alice: singleton( Dummy ) } )
-		expect( spy ).toBeCalledTimes( 1 )
-		expect( actual.get( ( p ) => p.alice ) ).toBeInstanceOf( SingletonLifetime )
-	} )
-	test( 'addScoped', () => {
-		const sut = Services()
-		const spy = jest.spyOn( sut, 'add' )
-		const actual = sut.add( { alice: scoped( Dummy ) } )
-		expect( spy ).toBeCalledTimes( 1 )
-		expect( actual.get( ( p ) => p.alice ) ).toBeInstanceOf( ScopedLifetime )
-	} )
-	test( 'addTransient', () => {
-		const sut = Services()
-		const spy = jest.spyOn( sut, 'add' )
-		const actual = sut.add( { alice: transient( Dummy ) } )
-		expect( spy ).toBeCalledTimes( 1 )
-		expect( actual.get( ( p ) => p.alice ) ).toBeInstanceOf( TransientLifetime )
-	} )
-	test( 'addStateful', () => {
-		const sut = Services()
-		const spy = jest.spyOn( sut, 'add' )
-		const actual = sut.add( { alice: stateful( Dummy ) } )
-		expect( spy ).toBeCalledTimes( 1 )
-		expect( actual.get( ( p ) => p.alice ) ).toBeInstanceOf( StatefulLifetime )
-		expect( actual.get( 'alice' )?.provide( new ScopedServiceProvider( Context() ) ) ).toHaveProperty( 'create' )
-	} )
-	test( 'Adding twice should give duplicate error', () => {
-		const sut = Services().add( { a: singleton( Dummy ) } )
-		// @ts-ignore
-		expect( () => sut.add( { a: singleton( Dummy ) } ) )
-			.toThrowError( new DuplicateDependencyError( 'a' ) )
-	} )
+  test( 'add', () => {
+    const sut = Services().add( { a: singleton( Dummy ) } )
+    expect( sut.get( 'a' ) ).toBeTruthy()
+  } )
+  test( 'addSingleton', () => {
+    const sut = Services()
+    const spy = jest.spyOn( sut, 'add' )
+    const actual = sut.add( { alice: singleton( Dummy ) } )
+    expect( spy ).toBeCalledTimes( 1 )
+    expect( actual.get( ( p ) => p.alice ) ).toBeInstanceOf( SingletonLifetime )
+  } )
+  test( 'addScoped', () => {
+    const sut = Services()
+    const spy = jest.spyOn( sut, 'add' )
+    const actual = sut.add( { alice: scoped( Dummy ) } )
+    expect( spy ).toBeCalledTimes( 1 )
+    expect( actual.get( ( p ) => p.alice ) ).toBeInstanceOf( ScopedLifetime )
+  } )
+  test( 'addTransient', () => {
+    const sut = Services()
+    const spy = jest.spyOn( sut, 'add' )
+    const actual = sut.add( { alice: transient( Dummy ) } )
+    expect( spy ).toBeCalledTimes( 1 )
+    expect( actual.get( ( p ) => p.alice ) ).toBeInstanceOf( TransientLifetime )
+  } )
+  test( 'addStateful', () => {
+    const sut = Services()
+    const spy = jest.spyOn( sut, 'add' )
+    const actual = sut.add( { alice: stateful( Dummy ) } )
+    expect( spy ).toBeCalledTimes( 1 )
+    expect( actual.get( ( p ) => p.alice ) ).toBeInstanceOf( StatefulLifetime )
+    expect( actual.get( 'alice' )?.provide( new ScopedServiceProvider( Context() ) ) ).toHaveProperty( 'create' )
+  } )
+  test( 'Adding twice should give duplicate error', () => {
+    const sut = Services().add( { a: singleton( Dummy ) } )
+    // @ts-ignore
+    expect( () => sut.add( { a: singleton( Dummy ) } ) ).toThrowError( new DuplicateDependencyError( 'a' ) )
+  } )
 } )
 
 describe( services.remove, () => {
-	test( 'Service is removed', () => {
-		const sut = Services().add( { a: singleton( Dummy ) } )
-		const actual = sut.remove( 'a' )
-		expect( actual.get( 'a' as any ) ).toBeUndefined()
-	} )
-	test( 'Service is removed and re-added', () => {
-		const sut = Services().add( { a: singleton( Dummy ) } ).remove( 'a' )
-		const actual = sut.add( { a: singleton( Dummy ) } )
-		expect( actual.get( 'a' ) ).toBeTruthy()
-	} )
+  test( 'Service is removed', () => {
+    const sut = Services().add( { a: singleton( Dummy ) } )
+    const actual = sut.remove( 'a' )
+    expect( actual.get( 'a' as any ) ).toBeUndefined()
+  } )
+  test( 'Service is removed and re-added', () => {
+    const sut = Services()
+      .add( { a: singleton( Dummy ) } )
+      .remove( 'a' )
+    const actual = sut.add( { a: singleton( Dummy ) } )
+    expect( actual.get( 'a' ) ).toBeTruthy()
+  } )
 } )
 
 describe( services.buildMock, () => {
-	test( 'Default mock strategy should be followed', () => {
-		const { a, sut } = dummy()
-			.add( {
-				a: singleton( dummyClass() ),
-				sut: singleton( dummyClass( ( { a } ) => ({ a }) ) ),
-			} )
-			.mock()
-		expect( a.id ).toBeTruthy()
-		expect( sut.a.id ).toBeNull()
-	} )
-	
-	test( 'Given mock strategy should be followed', () => {
-		const { sut } = dummy()
-			.add( {
-				a: singleton( dummyClass() ),
-				sut: singleton( dummyClass( ( { a } ) => ({ a }) ) ),
-			} )
-			.mock( {}, MockStrategy.exceptionStub )
-		expect( () => sut.a.id ).toThrowError( new ShouldBeMockedDependencyError( 'a', 'id', 'get' ) )
-	} )
-	
-	test( 'Dependency mock strategy should be followed', () => {
-		const { sut } = dummy()
-			.add( {
-				a: singleton( dummyClass() ),
-				b: singleton( dummyClass() ),
-				c: singleton( dummyClass() ),
-				sut: singleton( dummyClass( ( { a, b, c } ) => ({ a, b, c }) ) ),
-			} )
-			.mock( {
-				a: MockStrategy.dummyStub,
-				b: MockStrategy.exceptionStub,
-				c: { id: UUID.randomUUID() },
-			} )
-		
-		expect( sut.a.id ).toBeNull()
-		expect( () => sut.b.id ).toThrowError( new ShouldBeMockedDependencyError( 'b', 'id', 'get' ) )
-		expect( sut.c.id ).toBeTruthy()
-		sut.c.id = 'cake'
-		expect( sut.c.id ).toBe( 'cake' )
-	} )
-	
-	test( 'Property mock strategy should be followed', () => {
-		const { sut } = dummy()
-			.add( {
-				a: singleton( dummyClass() ),
-				sut: singleton( dummyClass( ( { a } ) => ({ a }) ) ),
-			} )
-			.mock( {
-				a: {
-					id: MockStrategy.nullStub,
-					func: MockStrategy.exceptionStub,
-					get getter() { return UUID.randomUUID() },
-				},
-			} )
-		expect( sut.a.id ).toBeNull()
-		expect( () => sut.a.func() ).toThrowError( new ShouldBeMockedDependencyError( 'a', 'func', 'function' ) )
-		expect( sut.getter ).toBeTruthy()
-	} )
-	
-	test( 'Stateful instance should be given mocked dependencies', () => {
-		const sut = dummy()
-			.add( {
-				scoped: scoped( dummyClass( ( { stateful } ) => ({ stateful }) ) ),
-				stateful: stateful( dummyClass( ( { stateful, scoped } ) => ({ stateful, scoped }) ) ),
-			} )
-			.mock( MockStrategy.exceptionStub )
-			.stateful
-		expect( () => sut.create().scoped.stateful )
-			.toThrowError( new ShouldBeMockedDependencyError( 'scoped', 'stateful', 'get' ) )
-	} )
-	
-	test( 'Service with Stateful dependency should be given mocked Stateful', () => {
-		const sut = dummy()
-			.add( {
-				scoped: scoped( dummyClass( ( { stateful } ) => ({ stateful }) ) ),
-				stateful: stateful( dummyClass( ( { stateful, scoped } ) => ({ stateful, scoped }) ) ),
-			} )
-			.mock( MockStrategy.exceptionStub )
-			.scoped
-		expect( () => sut.stateful.create() )
-			.toThrowError( new ShouldBeMockedDependencyError( 'stateful', 'create', 'function' ) )
-	} )
+  test( 'Default mock strategy should be followed', () => {
+    const { a, sut } = dummy()
+      .add( {
+        a: singleton( dummyClass() ),
+        sut: singleton( dummyClass( ( { a } ) => ( { a } ) ) ),
+      } )
+      .mock()
+    expect( a.id ).toBeTruthy()
+    expect( sut.a.id ).toBeNull()
+  } )
+
+  test( 'Given mock strategy should be followed', () => {
+    const { sut } = dummy()
+      .add( {
+        a: singleton( dummyClass() ),
+        sut: singleton( dummyClass( ( { a } ) => ( { a } ) ) ),
+      } )
+      .mock( {}, MockStrategy.exceptionStub )
+    expect( () => sut.a.id ).toThrowError( new ShouldBeMockedDependencyError( 'a', 'id', 'get' ) )
+  } )
+
+  test( 'Dependency mock strategy should be followed', () => {
+    const { sut } = dummy()
+      .add( {
+        a: singleton( dummyClass() ),
+        b: singleton( dummyClass() ),
+        c: singleton( dummyClass() ),
+        sut: singleton( dummyClass( ( { a, b, c } ) => ( { a, b, c } ) ) ),
+      } )
+      .mock( {
+        a: MockStrategy.dummyStub,
+        b: MockStrategy.exceptionStub,
+        c: { id: UUID.randomUUID() },
+      } )
+
+    expect( sut.a.id ).toBeNull()
+    expect( () => sut.b.id ).toThrowError( new ShouldBeMockedDependencyError( 'b', 'id', 'get' ) )
+    expect( sut.c.id ).toBeTruthy()
+    sut.c.id = 'cake'
+    expect( sut.c.id ).toBe( 'cake' )
+  } )
+
+  test( 'Property mock strategy should be followed', () => {
+    const { sut } = dummy()
+      .add( {
+        a: singleton( dummyClass() ),
+        sut: singleton( dummyClass( ( { a } ) => ( { a } ) ) ),
+      } )
+      .mock( {
+        a: {
+          id: MockStrategy.nullStub,
+          func: MockStrategy.exceptionStub,
+          get getter() {
+            return UUID.randomUUID()
+          },
+        },
+      } )
+    expect( sut.a.id ).toBeNull()
+    expect( () => sut.a.func() ).toThrowError( new ShouldBeMockedDependencyError( 'a', 'func', 'function' ) )
+    expect( sut.getter ).toBeTruthy()
+  } )
+
+  test( 'Stateful instance should be given mocked dependencies', () => {
+    const sut = dummy()
+      .add( {
+        scoped: scoped( dummyClass( ( { stateful } ) => ( { stateful } ) ) ),
+        stateful: stateful( dummyClass( ( { stateful, scoped } ) => ( { stateful, scoped } ) ) ),
+      } )
+      .mock( MockStrategy.exceptionStub ).stateful
+    expect( () => sut.create().scoped.stateful ).toThrowError(
+      new ShouldBeMockedDependencyError( 'scoped', 'stateful', 'get' ),
+    )
+  } )
+
+  test( 'Service with Stateful dependency should be given mocked Stateful', () => {
+    const sut = dummy()
+      .add( {
+        scoped: scoped( dummyClass( ( { stateful } ) => ( { stateful } ) ) ),
+        stateful: stateful( dummyClass( ( { stateful, scoped } ) => ( { stateful, scoped } ) ) ),
+      } )
+      .mock( MockStrategy.exceptionStub ).scoped
+    expect( () => sut.stateful.create() ).toThrowError(
+      new ShouldBeMockedDependencyError( 'stateful', 'create', 'function' ),
+    )
+  } )
+
+  test( 'Service with MockStrategy realValue should return the real deal', () => {
+    const sut = dummy()
+      .add( {
+        a: singleton( dummyClass( () => ( {} ) ) ),
+        b: singleton( dummyClass( () => ( {} ) ) ),
+        c: singleton( dummyClass( ( { a, b } ) => ( { a, b } ) ) ),
+      } )
+      .mock( {
+        a: MockStrategy.realValue,
+        b: MockStrategy.nullStub,
+      } ).c
+
+    expect( sut.a.id ).toBeTruthy()
+    expect( sut.b.id ).toBeNull()
+  } )
 } )
